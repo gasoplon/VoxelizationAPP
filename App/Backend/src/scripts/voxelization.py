b680f2966f271faf101a21e9b1d77750166a3e82
davidominguez <56188679+davidominguez@users.noreply.github.com>
2022-03-18 19:13:13 +0100

    ENV gitignore

--- App/Backend/src/scripts/voxelization.py
+++ App/Backend/src/scripts/voxelization.py
@@ -1,38 +1,3 @@
-
-##################################################################################################################
-# import trimesh
-# from trimesh.voxel import creation
-# import os
-
-# print(trimesh.available_formats())
-# mesh = trimesh.exchange.load.load(open("../../../../Examples/Cuerpo.obj","r"), "obj")
-# print(mesh)
-
-# voxelized = creation.local_voxelize(mesh, mesh.centroid , mesh.extents.max() / 128, 64, fill=True)
-##################################################################################################################
-
-# import open3d as o3d
-
-# textured_mesh = o3d.io.read_triangle_mesh("../../../../Examples/Cuerpo.obj",print_progress=True)
-# print(type(textured_mesh))
-##################################################################################################################
-
-# import pywavefront
-
-# scene = pywavefront.Wavefront("../../../../Examples/Cuerpo.obj")
-
-# for name, material in scene.materials.items():
-#     # Contains the vertex format (string) such as "T2F_N3F_V3F"
-#     # T2F, C3F, N3F and V3F may appear in this string
-#     print(material.vertex_format)
-#     # Contains the vertex list of floats in the format described above
-#     print(type(material.vertices))
-#     # Material properties
-#     material.diffuse
-#     material.ambient
-#     material.texture
-##################################################################################################################
-
 import bpy
 import sys
 
@@ -44,6 +9,9 @@ errores = [{
     'code': 1,
     'desc': DESC_FORMAT.format(1, 'Existe mas de un objeto en el archivo OBJ')
 }]
+
+# Otros objetos de la escena
+others_objs = ['Camera', 'Cube', 'Light']
 
 argv = sys.argv
 argv = argv[argv.index("--") + 1:] # get all args after "--"
@@ -57,12 +25,17 @@ bpy.ops.import_scene.obj(filepath=obj_in, axis_forward='-Z', axis_up='Y')
 
 # Objects
 # TODO: Comprobar que en la escena solo deba existir un elemento(o 4 que no sean Camera, Light o Cube) y otras figuras
-obj_name = bpy.data.objects.keys()
-if(len(obj_name) != 1):
-    print(errores[1-1]['desc'])
+obj_names = bpy.data.objects.keys()
+obj_name = None
+for o in obj_names:
+    if(o not in others_objs):
+        obj_name = o
+
+if(len(obj_names) != 4 or obj_name == None):
+    print(errores[0]['desc'])
     exit(1)
 
-obj_selected = bpy.data.objects[0]
+obj_selected = bpy.data.objects[obj_name]
 
 # Remesh(Voxelization).Apply modifier
 modifier = obj_selected.modifiers.new(name="Remesh", type='REMESH')
--- App/Backend/src/utils.py
+++ App/Backend/src/utils.py
@@ -13,6 +13,7 @@ BLENDER_COMMAND = 'blender --background --factory-startup --python ./scripts/vox
 # Logger
 logger = logging.getLogger(__name__)
 
+
 def allowed_file_extension(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -22,20 +23,26 @@ def checkFileUploaded(files):
     # Get file
     file = files[FILE_NAME]
     logger.debug(file)
-        
+
     if not allowed_file_extension(file.filename):
-        raise InvalidAPIParameterException(ERROR_CODES.NOT_ALLOWED_FILE_EXTENSION_ERROR_012)
-    
+        raise InvalidAPIParameterException(
+            ERROR_CODES.NOT_ALLOWED_FILE_EXTENSION_ERROR_012)
+
     return file
 
 # METODOS DE VOXELIZACION
-def voxelization(file_name, resolution = 4, removeDisconnectedElements = False):
+
+
+def voxelization(file_name, resolution=4, removeDisconnectedElements=False):
     # os.system(BLENDER_COMMAND.format(config['DIRECTORY_UPLOADED_FILE'] +'/'+ file_name, config['FILES_PROCESSED'] + '/' + file_name))
-    formatted_command = BLENDER_COMMAND.format(config['DIRECTORY_UPLOADED_FILE'] +'/'+ file_name, config['FILES_PROCESSED'] + '/' + file_name, resolution, removeDisconnectedElements)
+    formatted_command = BLENDER_COMMAND.format(
+        config['DIRECTORY_UPLOADED_FILE'] + '/' + file_name, config['FILES_PROCESSED'] + '/' + file_name, resolution, removeDisconnectedElements)
     output = os.popen(formatted_command)
-    errors = findall("ERR_CODE: \d",output.read())
+    print(output.read())
+    errors = findall("ERR_CODE: \d", output.read())
     logger.error(errors)
     for e in errors:
         if('1' in e):
-            raise InvalidAPIParameterException(ERROR_CODES.NO_SINGLE_ELEMENT_IN_FILE_ERROR_030)
+            raise InvalidAPIParameterException(
+                ERROR_CODES.NO_SINGLE_ELEMENT_IN_FILE_ERROR_030)
     return None
