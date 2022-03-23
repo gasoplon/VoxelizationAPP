ac3b8747b5549f6a9e8b745289bb2f7bcbc7756e
davidominguez <56188679+davidominguez@users.noreply.github.com>
2022-03-23 15:55:58 +0100

    Changes

--- App/Frontend/voxelization-front/src/components/RenderPanel.jsx
+++ App/Frontend/voxelization-front/src/components/RenderPanel.jsx
@@ -1,26 +1,36 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Slider, Checkbox, FormControlLabel, Box } from "@mui/material";
-import SelectObject from "./SelectObject";
 import RenderBox from "./RenderBox";
 import * as Constants from "../constants.js";
 import SelectListObject from "./SelectListObject";
+import { v4 as uuidv4 } from "uuid";
 
 export function RenderPanel() {
   // ------------------- ESTADOS -----------------------------------------
-  // Panel configuracion
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [selectedObject, setSelectedObject] = useState(
+  // PANEL DE CONF
+  // Input(subida de archivos)
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+  // Path del objeto seleccionado
+  const [selectedPathFile, setSelectedPathFile] = useState(
     Constants.DEFAULT_MODEL_PATH
   );
   const [resolutionVoxel, setResolutionVoxel] = useState(
     Constants.DEFAULT_VOXELIZATION_RESOLUTION
   );
   const [useRemoveDisconnected, setUseRemoveDisconnected] = useState(true);
-  // Errores
+  // ERRORES
   const [errores, setErrors] = useState("");
+  // ARCHIVOS
+  // Estado con la estructura de datos necesaria
+  const [filesUploadedItems, setFilesUploadedItems] = React.useState(
+    initJSONDataFileStructure()
+  );
 
   // ------------------- MANEJADORES ---------------------------------------
+  const handleFileUploaded = (event) => {
+    console.log(event.target.files[0]);
+  };
   const handleResolutionChange = (event, newValue) => {
     setResolutionVoxel(newValue);
   };
@@ -28,21 +38,19 @@ export function RenderPanel() {
     setUseRemoveDisconnected(newValue);
   };
   const onFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
+    setUploadedFiles(...uploadedFiles, event.target.files[0]);
   };
   const handleObjectChange = (value) => {
-    setSelectedObject(
-      Constants.ROOT_MODELS_DEMOS_PATH + value + Constants.DEMOS_EXTENSION
-    );
+    setSelectedPathFile(value);
   };
   // On file upload (click the upload button)
   const onFileUpload = () => {
-    if (selectedFile === null) return;
+    if (uploadedFiles === null) return;
     // Create an object of formData
     const formData = new FormData();
 
     // Update the formData object with file
-    formData.append("fileUploaded", selectedFile, selectedFile.name);
+    formData.append("fileUploaded", uploadedFiles, uploadedFiles.name);
 
     // Update the formData object with resolution
     formData.append("resolutionVoxel", resolutionVoxel);
@@ -51,7 +59,7 @@ export function RenderPanel() {
     formData.append("useRemoveDisconnected", useRemoveDisconnected);
 
     // Details of the uploaded file
-    console.log(selectedFile);
+    console.log(uploadedFiles);
 
     // Request made to the backend api
     // Send formData object
@@ -72,36 +80,74 @@ export function RenderPanel() {
         // setErrors(error["message"]);
       });
   };
+  // ------------------- FUNCIONES AUXILIARES ---------------------------------------
+  function addFileStructureToState(index, newDataStructure) {
+    var newState = { ...filesUploadedItems };
+    newState.files[index] = newDataStructure;
+    newState.numElements++;
+    setFilesUploadedItems(newState);
+  }
+  function createFileDataStructure(fileName, extension, prePath, isDemoFile) {
+    var jsonObj = {};
+    jsonObj.id = uuidv4();
+    jsonObj.fileName = fileName;
+    jsonObj.pathFile = prePath + fileName + extension;
+    jsonObj.isDemo = isDemoFile;
+    return jsonObj;
+  }
+  function initJSONDataFileStructure() {
+    // Estructura global
+    var jsonObj = {};
+    jsonObj.numElements = 0;
+    jsonObj.files = {};
 
-  // ------------------- DETALLES DE ARCHIVO ---------------------------------------
-  const fileData = () => {
-    if (selectedFile) {
-      return (
-        <div>
-          <h2>Detalles de archivo:</h2>
-          <p>Nombre del archivo: {selectedFile.name}</p>
-          <p>Tipo de archivo: {selectedFile.type}</p>
-          <p>
-            Última modificación: {selectedFile.lastModifiedDate.toDateString()}
-          </p>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <br />
-          <h4>¡No ha seleccionado un archivo aún!</h4>
-        </div>
-      );
+    // Demos data
+    if (Constants.DEMOS_MODELS) {
+      Constants.DEMOS_MODELS.forEach((value) => {
+        // Index
+        const index = Constants.DEMOS_MODELS.indexOf(value, 0);
+        // Crear estructura del archivo actual
+        var newDS = createFileDataStructure(
+          value,
+          ".obj",
+          Constants.ROOT_MODELS_DEMOS_PATH,
+          true
+        );
+        jsonObj.files[index] = newDS;
+        jsonObj.numElements++;
+      });
     }
-  };
+    return jsonObj;
+  }
+  // ------------------- DETALLES DE ARCHIVO ---------------------------------------
+  // const fileData = () => {
+  //   if (uploadedFiles) {
+  //     return (
+  //       <div>
+  //         <h2>Detalles de archivo:</h2>
+  //         <p>Nombre del archivo: {uploadedFiles.name}</p>
+  //         <p>Tipo de archivo: {uploadedFiles.type}</p>
+  //         <p>
+  //           Última modificación: {uploadedFiles.lastModifiedDate.toDateString()}
+  //         </p>
+  //       </div>
+  //     );
+  //   } else {
+  //     return (
+  //       <div>
+  //         <br />
+  //         <h4>¡No ha seleccionado un archivo aún!</h4>
+  //       </div>
+  //     );
+  //   }
+  // };
 
   // ------------------- RETURN ---------------------------------------
   return (
     <div
       style={{ border: "1px solid black", margin: "5px", textAlign: "center" }}
     >
-      <RenderBox selectedModelPath={selectedObject}></RenderBox>
+      <RenderBox selectedModelPath={selectedPathFile}></RenderBox>
       <h1>Componente de carga(Pruebas de Algoritmo)</h1>
       <br />
       <h3>Resolución:</h3>
@@ -122,14 +168,17 @@ export function RenderPanel() {
           }
           label="Eliminar elementos inconexos."
         />
-        <SelectObject
+        {/* <SelectObject
           setObjectSelected={handleObjectChange}
-          render={selectedFile ? true : false}
-        ></SelectObject>
-        <SelectListObject></SelectListObject>
+          render={uploadedFile ? true : false}
+        ></SelectObject> */}
+        <SelectListObject
+          // onChangeFileSelected={handleObjectChange}
+          filesUploaded={filesUploadedItems}
+        ></SelectListObject>
       </Box>
       <div>
-        <input type="file" onChange={onFileChange} />
+        <input type="file" onChange={handleFileUploaded} />
         <button onClick={onFileUpload}>Upload!</button>
       </div>
       {errores && (
@@ -137,7 +186,7 @@ export function RenderPanel() {
           {errores}
         </h5>
       )}
-      {fileData()}
+      {/* {fileData()} */}
     </div>
   );
 }
--- App/Frontend/voxelization-front/src/components/SelectListObject.jsx
+++ App/Frontend/voxelization-front/src/components/SelectListObject.jsx
@@ -3,104 +3,68 @@ import Box from "@mui/material/Box";
 import List from "@mui/material/List";
 import ListItemButton from "@mui/material/ListItemButton";
 import IconButton from "@mui/material/IconButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Divider from "@mui/material/Divider";
-import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 import DeleteIcon from "@mui/icons-material/Delete";
 import Typography from "@mui/material/Typography";
 import * as Constants from "../constants.js";
-
-export default function SelectedListItem() {
+export default function SelectedListItem(props) {
   // ------------------- ESTADOS -----------------------------------------
-  // Estado con la estructura de datos necesaria
-  const [filesUploadedItems, setFilesUploadedItems] = React.useState(
-    initJSONDataFileStructure()
+
+  const [selectedIDFile, setSelectedIDFile] = React.useState(
+    props.filesUploaded.files[0].id
   );
-  const [selectedIndex, setSelectedIndex] = React.useState(0);
 
   // ------------------- MANEJADORES -----------------------------------------
-  const handleListItemClick = (event, index) => {
-    setSelectedIndex(index);
+  const handleListItemClick = (event, id) => {
+    setSelectedIDFile(id);
   };
   // ------------------- FUNCIONES AUXILIARES -----------------------------------------
-  function addFileStructureToState(index, newDataStructure) {
-    var newState = { ...filesUploadedItems };
-    newState.files[index] = newDataStructure;
-    newState.numElements++;
-    setFilesUploadedItems(newState);
-  }
 
-  function createFileDataStrcuture(fileName, extension, prePath, isDemoFile) {
-    var jsonObj = {};
-    jsonObj.fileName = fileName;
-    jsonObj.pathFile = prePath + fileName + extension;
-    jsonObj.isDemo = isDemoFile;
-    return jsonObj;
-  }
-
-  function initJSONDataFileStructure() {
-    // Estructura global
-    var jsonObj = {};
-    jsonObj.numElements = 0;
-    jsonObj.files = {};
-
-    // Demos data
-    if (Constants.DEMOS_MODELS) {
-      Constants.DEMOS_MODELS.forEach((value) => {
-        // Index
-        const index = Constants.DEMOS_MODELS.indexOf(value, 0);
-        // Crear estructura del archivo actual
-        var newDS = createFileDataStrcuture(
-          value,
-          ".obj",
-          Constants.ROOT_MODELS_DEMOS_PATH,
-          true
-        );
-        jsonObj.files[index] = newDS;
-        jsonObj.numElements++;
-      });
-    }
-    return jsonObj;
-  }
   // ------------------- ITEMS -----------------------------------------
   const DemosListItems = () => {
-    if (Constants.DEMOS_MODELS) {
-      return Constants.DEMOS_MODELS.map((value) => {
-        // Index
-        const index = Constants.DEMOS_MODELS.indexOf(value, 0);
-        // Return
-        return (
-          <ListItemButton
-            key={value}
-            selected={selectedIndex === index}
-            onClick={(event) => handleListItemClick(event, index)}
-          >
-            <ListItemText primary={value} />
-          </ListItemButton>
-        );
-      });
-    } else return <div></div>;
+    var files = props.filesUploaded.files;
+    var items = [];
+    if (files) {
+      for (let i = 0; i < Object.keys(files).length; i++) {
+        if (files[i].isDemo) {
+          items.push(
+            <ListItemButton
+              key={files[i].id}
+              selected={selectedIDFile === files[i].id}
+              onClick={(event) => handleListItemClick(event, files[i].id)}
+            >
+              <ListItemText primary={files[i].fileName} />
+            </ListItemButton>
+          );
+        }
+      }
+    }
+    return items;
   };
 
-  const SelectedFilesListItems = () => {
-    if (filesUploadedItems.length > 0) {
-      return filesUploadedItems.map((value) => {
-        const index = filesUploadedItems.indexOf(value, 0);
-        return (
-          <ListItemButton
-            key={value}
-            selected={selectedIndex === index}
-            onClick={(event) => handleListItemClick(event, index)}
-          >
-            <ListItemText primary={value} />
-            <IconButton edge="end" aria-label="delete">
-              <DeleteIcon />
-            </IconButton>
-          </ListItemButton>
-        );
-      });
-    } else return <div></div>;
+  const UploadedFilesItems = () => {
+    // var files = props.filesUploaded.files;
+    // var items = [];
+    // if (files) {
+    //   for (let i = 0; i < Object.keys(files).length; i++) {
+    //     if (!files[i].isDemo) {
+    //       items.push(
+    //         <ListItemButton
+    //           key={files[i].id}
+    //           selected={selectedIDFile === files[i].id}
+    //           onClick={(event) => handleListItemClick(event, files[i].id)}
+    //         >
+    //           <ListItemText primary={files[i].fileName} />
+    //           <IconButton edge="end" aria-label="delete">
+    //             <DeleteIcon />
+    //           </IconButton>
+    //         </ListItemButton>
+    //       );
+    //     }
+    //   }
+    // }
+    // return items;
   };
 
   // ------------------- RETURN -----------------------------------------
@@ -122,7 +86,7 @@ export default function SelectedListItem() {
         <Typography sx={{ mt: 1, mb: 1 }} variant="subtitle2" component="div">
           Archivos
         </Typography>
-        {SelectedFilesListItems()}
+        {UploadedFilesItems()}
       </List>
     </Box>
   );
