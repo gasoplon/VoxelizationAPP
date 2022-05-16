57893013cd5135dcc18aa9d94307390a1d02c3e9
davidominguezapata@outlook.es <davidominguezapata@outlook.es>
2022-05-16 20:33:22 +0200

    Cambio en renderizado

--- App/Frontend/voxelization-front/src/components/RenderBox.jsx
+++ App/Frontend/voxelization-front/src/components/RenderBox.jsx
@@ -44,18 +44,41 @@ export function RenderBox(props) {
   // Configuracion
   const configuracion = {
     sceneBackgroundColor: 0xbfe3dd,
+    intensidadDeAmbiente: 0.3,
+    colorAmbiente: 0xffffff,
+    intensidadDirecta: 0.8 * Math.PI,
+    colorDirecto: 0xffffff,
   };
+
+  // Lights
+  const luzGlobal = new HemisphereLight();
+  const luz1 = new AmbientLight(
+    configuracion.colorAmbiente,
+    configuracion.intensidadDeAmbiente
+  );
+  const luz2 = new DirectionalLight(
+    configuracion.colorDirecto,
+    configuracion.intensidadDirecta
+  );
 
   // Valores iniciales
   scene.add(camera);
+  scene.add(luzGlobal);
+  camera.add(luz1);
+  camera.add(luz2);
+  luz2.position.set(0.5, 0, 0.87);
   scene.background = new Color(configuracion.sceneBackgroundColor);
   renderer.physicallyCorrectLights = true;
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   controls.screenSpacePanning = true;
-  // requestAnimationFrame(animateFrame);
 
   /****************************************************************************************** */
+  // Rendeizar frame
+  const render = () => {
+    renderer.render(scene, camera);
+  };
+
   // Repintar frame
   const animateFrame = () => {
     // Frame
@@ -68,21 +91,16 @@ export function RenderBox(props) {
     render();
   };
 
-  // Rendeizar frame
-  const render = () => {
-    renderer.render(scene, camera);
-  };
-
   // Resize de la pantalla actual
   const resize = () => {
-    // Get size del elemento padre
-    const { clientHeight, clientWidth } = this.parentElement;
-
-    // Actualizar
-    camera.aspect = clientWidth / clientHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(clientWidth, clientHeight);
+    // // Get size del elemento padre
+    // const { clientHeight, clientWidth } = this.parentElement;
+    // // Actualizar
+    // camera.aspect = clientWidth / clientHeight;
+    // camera.updateProjectionMatrix();
+    // renderer.setSize(clientWidth, clientHeight);
   };
+
   window.addEventListener("resize", resize, false);
 
   const loadObject = () => {
@@ -118,9 +136,17 @@ export function RenderBox(props) {
       );
   };
 
+  const clear = () => {
+    // Eliminamos el anterior
+    if (canvasRef.current.children[0] !== undefined)
+      canvasRef.current.removeChild(canvasRef.current.children[0]);
+  };
+
   const setContent = (GLFTScene) => {
     // Limpiar escena
-    this.clear();
+    clear();
+
+    canvasRef.current.appendChild(renderer.domElement);
 
     // Caja adaptada al objeto
     const boxObject = new Box3().setFromObject(GLFTScene);
@@ -141,33 +167,44 @@ export function RenderBox(props) {
     camera.far = sizeBox * 100;
     camera.updateProjectionMatrix();
 
+    camera.position.copy(centerBox);
+    camera.position.x += sizeBox / 2.0;
+    camera.position.y += sizeBox / 5.0;
+    camera.position.z += sizeBox / 2.0;
+    camera.lookAt(centerBox);
+
     // Añadir el objeto a la escena
     scene.add(GLFTScene);
+    window.content = GLFTScene;
   };
 
   // componentDidMount componentDidUpdate
-  // useEffect(() => {
-  //   // Eliminamos el anterior
-  //   if (canvasRef.current.children[0] !== undefined)
-  //     canvasRef.current.removeChild(canvasRef.current.children[0]);
+  useEffect(() => {
+    // Eliminamos el anterior
+    if (canvasRef.current.children[0] !== undefined)
+      canvasRef.current.removeChild(canvasRef.current.children[0]);
 
-  //   // Render
-  //   canvasRef.current.appendChild(renderer.domElement);
+    // Render
+    canvasRef.current.appendChild(renderer.domElement);
 
-  //   // Luz
-  //   // const light = new HemisphereLight(0xffffbb, 0x080820, 1);
-  //   // scene.add(light);
+    // Luz
+    // const light = new HemisphereLight(0xffffbb, 0x080820, 1);
+    // scene.add(light);
 
-  //   // Plano
-  //   scene.add(new GridHelper(40, 10, 0x888888, 0x444444));
+    // Plano
+    // scene.add(new GridHelper(40, 10, 0x888888, 0x444444));
 
-  //   // OBJ Loader
-  //   loadObject();
-  //   // console.log(scene);
-  //   // Animar
-  //   animate();
-  // }, [props.selectedModel]);
+    // OBJ Loader
+    loadObject();
+    // console.log(scene);
+    // Animar
+    animateFrame();
+  }, [props.selectedModel]);
 
+  // Comenzar renderizado
+  requestAnimationFrame(animateFrame);
+
+  // Devolver componente
   return <div ref={canvasRef} />;
 }
 
