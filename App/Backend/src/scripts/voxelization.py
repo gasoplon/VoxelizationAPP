e9a43d699150d89563cddbccaaeebcf6a4bc2627
davidominguezapata@outlook.es <davidominguezapata@outlook.es>
2022-05-18 18:42:39 +0200

    A falta de remeshed UVs

--- App/Backend/src/scripts/voxelization.py
+++ App/Backend/src/scripts/voxelization.py
@@ -13,7 +13,7 @@ OTHERS_OBJS = ['Camera', 'Cube', 'Light']  # Otros objetos de la escena
 ANGLE_LIMIT = 1.15191731  # 66º
 ITERATIONS_SUBDIVIDE = 7  # Nº de iteraciones del subdivide
 APPLY_MODIFIERS = {
-    "subdivide": False,
+    "subdivide": True,
     "remesh": True,
     "generateUVs": True,
     "shrinkWrap": False,
@@ -22,21 +22,30 @@ APPLY_MODIFIERS = {
 # /////////////////  FUNCS AUXS.  ////////////////////////////
 
 
-def selectAllObjects(objectsList):
-    for o in objectsList:
-        select_one_object(o, False)
+def setActive(obj):
+    bpy.context.view_layer.objects.active = obj
 
 
-def select_one_object(object_selected, deselectOthersObjects=True):
-    if(deselectOthersObjects):
-        bpy.ops.object.select_all(action='DESELECT')
-    bpy.context.view_layer.objects.active = object_selected
+def selectAllObjects():
+    setActive(None)
+    for o in all_objects:
+        o.select_set(True)
+
+
+def deselectAllObjects():
+    setActive(None)
+    for o in all_objects:
+        o.select_set(False)
+
+
+def select_one_object(object_selected):
+    deselectAllObjects()
+    setActive(object_selected)
     object_selected.select_set(True)
 
 
 def remove_object(object):
-    select_one_object(object)
-    bpy.ops.object.delete()
+    bpy.data.objects.remove(object)
 
 
 def copy_object(source_object, new_name):
@@ -47,10 +56,6 @@ def copy_object(source_object, new_name):
     remove_UVs(new_obj)
     bpy.context.collection.objects.link(new_obj)
     return new_obj
-
-
-# def set_active_object(object):
-#     bpy.context.selected_objects = object
 
 
 def remove_UVs(object):
@@ -81,25 +86,31 @@ obj_name = None
 for o in obj_names:
     if(o not in OTHERS_OBJS):
         obj_name = o
+    else:
+        remove_object(bpy.data.objects[o])
 
 # Comprobar que solo existe 1 objeto en la escena
 if(len(obj_names) != 4 or obj_name == None):
     print(ERROR_NOT_ONE_ELEMENT_ON_SCENE['desc'])
     exit(ERROR_NOT_ONE_ELEMENT_ON_SCENE['code'])
 
+for area in bpy.context.screen.areas:
+    if area.type == 'OUTLINED':
+        space_data = area.spaces.active
+        break
 
 # Copias para ser modificadas
 # Objetos
 original_object = bpy.data.objects[obj_name]
 shrinkwrapped_object = copy_object(original_object, "Shrinkwrapped_Object")
 remeshed_object = copy_object(original_object, "Remeshed_Object")
+all_objects = [shrinkwrapped_object, remeshed_object]
+
+# Remove useless objects
 remove_object(original_object)
 
 # BMeshes
 bmesh_shrinkwrapped_object = bmesh.new()
-
-# Control
-generateUVsDone = False  # Para hacer el bake se precisa de las UVs
 
 # Add extra vertices
 if(APPLY_MODIFIERS["subdivide"]):
@@ -123,13 +134,19 @@ if(APPLY_MODIFIERS["remesh"]):
     bpy.ops.object.modifier_apply(
         modifier=modifierRemesh.name
     )
+    deselectAllObjects()
 
 
 # Generate UV (Smart UV Project) from meshed object
 if(APPLY_MODIFIERS["generateUVs"]):
+    selectAllObjects()
+    # setActive(remeshed_object)
     bpy.ops.object.editmode_toggle()
+    # print(bpy.context.selected_objects)
+    # print(bpy.context.active_object)
     bpy.ops.uv.smart_project(angle_limit=ANGLE_LIMIT)
-    generateUVsDone = True
+    bpy.ops.object.editmode_toggle()
+    deselectAllObjects()
 
 # Shrinkwrap
 if(APPLY_MODIFIERS["shrinkWrap"]):
@@ -138,13 +155,12 @@ if(APPLY_MODIFIERS["shrinkWrap"]):
         type='SHRINKWRAP', name="Shrinkwrap")
     modifierShrinkwrap.target = remeshed_object
     modifierShrinkwrap.wrap_method = 'NEAREST_SURFACEPOINT'
-    bpy.ops.object.editmode_toggle()
     bpy.ops.object.modifier_apply(
         modifier=modifierShrinkwrap.name
     )
 
 # Bake
-if(APPLY_MODIFIERS["bake"] and (generateUVsDone or not APPLY_MODIFIERS["generateUVs"])):
+if(APPLY_MODIFIERS["bake"]):
 
     image_name = remeshed_object.name + '_BakedTexture'
     texture_image = bpy.data.images.new(image_name, 1024, 1024)
@@ -172,14 +188,14 @@ if(APPLY_MODIFIERS["bake"] and (generateUVsDone or not APPLY_MODIFIERS["generate
     select_one_object(remeshed_object)
 
     # Bake
-    bpy.ops.object.bake(type="DIFFUSE", pass_filter={
-        "COLOR"}, use_selected_to_active=True, margin=0)
+    # bpy.ops.object.bake(type="DIFFUSE", pass_filter={
+    #     "COLOR"}, use_selected_to_active=True, margin=0)
 
     # Save texture image
     texture_image.save_render(filepath='E:\\Documentos\\Temporal\\baked.png')
 
 # Select all objets to export
-selectAllObjects([shrinkwrapped_object, remeshed_object])
+selectAllObjects()
 
 # Export objects
 bpy.ops.export_scene.gltf(
