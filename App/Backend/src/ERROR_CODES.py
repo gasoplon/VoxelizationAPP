ee768bd0fc9838de6632ad28eacf4d318dede79a
davidominguezapata@outlook.es <davidominguezapata@outlook.es>
2022-06-08 18:22:03 +0200

    Cambios y copia del cocumento

--- App/Backend/src/ERROR_CODES.py
+++ App/Backend/src/ERROR_CODES.py
@@ -9,7 +9,7 @@ SUCCESSFULL_001 = {'code': 1, 'message': '¡Proceso terminado correctamente!'}
 
 # API ERRORS PARAMETERS
 INVALID_RESOLUTION_RANGE_ERROR_010 = {
-    'code': 10, 'message': 'Debe ser un valor entero comprendido entre 1 y 8[ambos inclusive].'}
+    'code': 10, 'message': 'La resolución debe ser un valor entero comprendido entre 1 y 8[ambos inclusive].'}
 INVALID_RESOLUTION_TYPE_ERROR_011 = {
     'code': 11, 'message': 'La resolución debe ser un nº entero.'}
 NOT_ALLOWED_FILE_EXTENSION_ERROR_012 = {
@@ -17,7 +17,7 @@ NOT_ALLOWED_FILE_EXTENSION_ERROR_012 = {
 MISSING_PARAMETERS_ERROR_013 = {'code': 13,
                                 'message': 'Falta algún parámetro: {}'}
 INVALID_REMOVE_DISCONNECTED_ELEMENTS_TYPE_ERROR_014 = {
-    'code': 14, 'message': 'remove_disconnected_elements debe ser un valor booleano: "True" o "False".'}
+    'code': 14, 'message': 'remove_disconnected_elements debe ser un valor booleano: "true" o "false".'}
 
 # API ERROR with OBJ file
 NO_SINGLE_ELEMENT_IN_FILE_ERROR_030 = {
